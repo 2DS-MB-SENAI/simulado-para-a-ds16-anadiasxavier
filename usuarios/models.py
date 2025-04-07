cf09686b5d8e25bd3b2c96e48f65fe14db90253f
Ana Clara Dias Xavier <54588459864@>
2025-04-07 11:35:29 -0300

    task3

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Usuarios(models.Model):
+class Usuarios(AbstractUser):
     telefone = models.CharField(max_length=15, null=True, blank=True)
     REQUIRED_FIELDS = ['telefone']
