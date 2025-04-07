3fe0327c53803cbfe4264b3ac202aaac0b86ebc8
Ana Clara Dias Xavier <54588459864@>
2025-04-07 10:37:10 -0300

    task2

--- livros/urls.py
+++ livros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('livros/', views.listar_livros, name='listar_livros')
+    path('livros/', views.listar_livros, name='listar_livros'),
+    path('api/livros/', views.listar),
 ]
